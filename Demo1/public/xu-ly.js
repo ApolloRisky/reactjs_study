5291135fad9d62d0e9300b054688a989299acc18
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2018-12-28 22:46:57 +0700

    Updates

--- Demo1/public/xu-ly.js
+++
@@ -1,7 +0,0 @@
-ReactDOM.render( 
-    <div>
-        <h1> DEMO REACT JS </h1>
-        <h1 className="blueBG"> This is blue text.</h1>
-    </div>, 
-    document.getElementById("root")
-    );
---
+++ precache-manifest.7868cce0edb543c2e76ff95a2af08e7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12112f7114ae6e842620",
+    "url": "/static/css/main.97341bc3.chunk.css"
+  },
+  {
+    "revision": "12112f7114ae6e842620",
+    "url": "/static/js/main.12112f71.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e5d0173c449da60a3303ed03b5dfb9c",
+    "url": "/index.html"
+  }
+];

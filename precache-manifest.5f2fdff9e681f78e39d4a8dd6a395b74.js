18dee50492da7f63b1eb1caa253bec577bd800b8
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2018-12-28 22:13:48 +0700

    Updates

---
+++ precache-manifest.5f2fdff9e681f78e39d4a8dd6a395b74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12112f7114ae6e842620",
+    "url": "/react-tutorial/static/css/main.97341bc3.chunk.css"
+  },
+  {
+    "revision": "12112f7114ae6e842620",
+    "url": "/react-tutorial/static/js/main.12112f71.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-tutorial/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "504e18bed47599844a493a41c1dec28f",
+    "url": "/react-tutorial/index.html"
+  }
+];
